beb8a47abe4a6d4b98be004858acd2186352016d
Lucas Pinheiro <pinheiro.lucash@gmail.com>
2024-04-19 20:55:44 -0300

    add js

--- main.js
+++ main.js
@@ -1,5 +1,26 @@
 const form = document.getElementById('formulario');
+let linhas = '';
 
 form.addEventListener('submit', function(e){
     e.preventDefault();
+    salvaContatos();
 });
+
+function salvaContatos() {
+    const nome = document.getElementById('nome');
+    const telefone = document.getElementById('telefone');
+
+    let linha = '<tr>';
+    linha += `<td>${nome.value}</td>`;
+    linha += `<td>${telefone.value}</td>`;
+    linha += `<td><img src="./Images/mobile.png" alt="telefone celular" /></td>` 
+    linha += `</tr>`;
+
+    linhas += linha;
+
+    const tabela = document.querySelector('tbody');
+    tabela.innerHTML = linhas;
+
+    nome.value = '';
+    telefone.value = '';
+}
